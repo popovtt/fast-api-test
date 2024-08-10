d9d458892d55415b6b0ac588f18e9efdba40a990
popovtt <piligrimqwert@gmail.com>
2024-08-10 12:34:41 +0300

    Create profile for user

---
+++ core/models/profile.py
@@ -0,0 +1,22 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .base import Base
+from .mixins import UserRelationMixin
+
+if TYPE_CHECKING:
+    from .user import User
+
+
+class Profile(UserRelationMixin, Base):
+    _user_id_unique = True
+    _user_back_populates = "profile"
+
+    first_name: Mapped[str | None] = mapped_column(String(40))
+    last_name: Mapped[str | None] = mapped_column(String(40))
+    bio: Mapped[str | None]
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), unique=True)
+
+    user: Mapped["User"] = relationship(back_populates="profile")
